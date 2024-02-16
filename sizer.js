7a7023060b869cfa53bb480c4e718de86f6f2f2b
Cedric <mcdougal.ce@gmail.com>
2024-02-16 09:57:34 -0500

    temp

--- sizer.js
+++ sizer.js
@@ -1,6 +1,6 @@
 const DIALOG_CONTAINER_SELECTOR = `.x1qjc9v5.x9f619.x78zum5.xdt5ytf.xl56j7k.x1c4vz4f.xg6iff7`;
 
-const interval = setInterval(() => {
+const resizeDialog = () => {
   const dialog = Array.from(document.querySelectorAll(`[role="dialog"]`)).find(
     (d) =>
       Array.from(d.querySelectorAll(`span`)).find(
@@ -55,26 +55,12 @@ const interval = setInterval(() => {
     parent.style.overflow = `visible`;
     parent = parent.parentElement;
   }
+};
 
-  // const dialogContainer = document.querySelector(DIALOG_CONTAINER_SELECTOR);
-  // const dialogContainerInner = dialogContainer?.childNodes[0];
-  // const dialog = dialogContainerInner?.childNodes[0];
-  // const dialogContent = dialog?.childNodes[dialog.childNodes.length - 1];
-  // const dialogContentForm = dialogContent?.childNodes[0];
-  // const photosContainer = dialogContentForm?.childNodes[0];
-  // const photosContainerInner = photosContainer?.childNodes[0];
-
-  // if (!dialogContainer || !dialogContainerInner || !dialog || !dialogContent) {
-  //   return;
-  // }
-
-  // dialogContainer.style.justifyContent = `stretch`;
-  // dialogContainerInner.style.flex = `1 1 auto`;
-  // dialogContainerInner.style.alignItems = `stretch`;
-  // dialog.style.flex = `1 1 auto`;
-  // dialog.style.maxHeight = `calc(100vh - 90px)`;
-  // dialogContent.style.width = `100%`;
-  // dialogContent.style.height = `100%`;
-  // dialogContentForm.style.height = `calc(100% - 80px)`;
-  // photosContainerInner.style.height = `100%`;
+const interval = setInterval(() => {
+  try {
+    resizeDialog();
+  } catch (err) {
+    // Do nothing
+  }
 }, 1000);
