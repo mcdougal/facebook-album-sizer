38d810ff4519fc4e0a4e03de1aa69f21fb580a61
Cedric <mcdougal.ce@gmail.com>
2024-02-16 09:32:21 -0500

    temp

--- sizer.js
+++ sizer.js
@@ -1,25 +1,80 @@
 const DIALOG_CONTAINER_SELECTOR = `.x1qjc9v5.x9f619.x78zum5.xdt5ytf.xl56j7k.x1c4vz4f.xg6iff7`;
 
 const interval = setInterval(() => {
-  const dialogContainer = document.querySelector(DIALOG_CONTAINER_SELECTOR);
-  const dialogContainerInner = dialogContainer?.childNodes[0];
-  const dialog = dialogContainerInner?.childNodes[0];
-  const dialogContent = dialog?.childNodes[dialog.childNodes.length - 1];
-  const dialogContentForm = dialogContent?.childNodes[0];
-  const photosContainer = dialogContentForm?.childNodes[0];
-  const photosContainerInner = photosContainer?.childNodes[0];
+  const dialog = Array.from(document.querySelectorAll(`[role="dialog"]`)).find(
+    (d) =>
+      Array.from(d.querySelectorAll(`span`)).find(
+        (s) => s.innerText.toLowerCase() === `add to album`
+      )
+  );
 
-  if (!dialogContainer || !dialogContainerInner || !dialog || !dialogContent) {
+  if (!dialog) {
     return;
   }
 
-  dialogContainer.style.justifyContent = `stretch`;
-  dialogContainerInner.style.flex = `1 1 auto`;
-  dialogContainerInner.style.alignItems = `stretch`;
-  dialog.style.flex = `1 1 auto`;
-  dialog.style.maxHeight = `calc(100vh - 90px)`;
-  dialogContent.style.width = `100%`;
-  dialogContent.style.height = `100%`;
-  dialogContentForm.style.height = `calc(100% - 80px)`;
-  photosContainerInner.style.height = `100%`;
+  const dialogContent = Array.from(dialog.childNodes).find((n) =>
+    n.querySelector(`form`)
+  );
+
+  const dialogForm = dialogContent?.querySelector(`form`);
+
+  if (!dialogContent || !dialogForm) {
+    return;
+  }
+
+  const scrollableArea = Array.from(dialogContent.querySelectorAll(`div`)).find(
+    (d) => getComputedStyle(d).overflowY === `auto`
+  );
+
+  const actionsContainer = Array.from(dialogForm.childNodes).find(
+    (n) =>
+      n.querySelector(`[role="button"]`)?.innerText.toLowerCase() ===
+      `add to album`
+  );
+
+  if (!scrollableArea || !actionsContainer) {
+    return;
+  }
+
+  dialog.style.position = `fixed`;
+  dialog.style.inset = 0;
+  dialogContent.style.width = `unset`;
+  dialogContent.style.height = `unset`;
+  scrollableArea.style.position = `fixed`;
+  scrollableArea.style.inset = `60px 8px 73px`;
+  scrollableArea.style.height = `unset`;
+  actionsContainer.style.position = `fixed`;
+  actionsContainer.style.left = `0`;
+  actionsContainer.style.right = `0`;
+  actionsContainer.style.bottom = `0`;
+
+  let parent = scrollableArea.parentElement;
+
+  while (parent && parent !== dialogContent) {
+    parent.style.position = `relative`;
+    parent.style.overflow = `visible`;
+    parent = parent.parentElement;
+  }
+
+  // const dialogContainer = document.querySelector(DIALOG_CONTAINER_SELECTOR);
+  // const dialogContainerInner = dialogContainer?.childNodes[0];
+  // const dialog = dialogContainerInner?.childNodes[0];
+  // const dialogContent = dialog?.childNodes[dialog.childNodes.length - 1];
+  // const dialogContentForm = dialogContent?.childNodes[0];
+  // const photosContainer = dialogContentForm?.childNodes[0];
+  // const photosContainerInner = photosContainer?.childNodes[0];
+
+  // if (!dialogContainer || !dialogContainerInner || !dialog || !dialogContent) {
+  //   return;
+  // }
+
+  // dialogContainer.style.justifyContent = `stretch`;
+  // dialogContainerInner.style.flex = `1 1 auto`;
+  // dialogContainerInner.style.alignItems = `stretch`;
+  // dialog.style.flex = `1 1 auto`;
+  // dialog.style.maxHeight = `calc(100vh - 90px)`;
+  // dialogContent.style.width = `100%`;
+  // dialogContent.style.height = `100%`;
+  // dialogContentForm.style.height = `calc(100% - 80px)`;
+  // photosContainerInner.style.height = `100%`;
 }, 1000);
